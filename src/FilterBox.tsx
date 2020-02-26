5986d2bd55caf32857f3e4e0e190e6bc977d3f72
thenakedthunder <balazs.radvanyi88@gmail.com>
2020-02-26 01:09:46 +0100

    nem

--- src/FilterBox.tsx
+++ src/FilterBox.tsx
@@ -1,18 +1,45 @@
-import React from "react";
-import { Dropdown } from "semantic-ui-react";
+import React, { ChangeEvent, useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
-export default function FilterBox(this: any) {
-  type FilterBoxProps = {
-    truckNames: string[];
-  };
+type FilterBoxProps = {
+  truckNames: string[];
+  onInputChange: (
+    event: ChangeEvent<{}>,
+    selected: string,
+    reason: "input" | "reset" | "clear"
+  ) => void;
+};
 
+const [selected, updateSelected] = useState([]);
+
+const handleInputChange: (
+  event: ChangeEvent<{}>,
+  selected: string,
+  reason: "input" | "reset" | "clear"
+) => {
+  updateSelected(selected);
+};
+
+export default function FilterBox(props: FilterBoxProps) {
   return (
-    <Dropdown
-      placeholder="Select Country"
-      fluid
-      search
-      selection
-      options={this.props.truckNames}
+    <Autocomplete
+      multiple={true}
+      value={selected}
+      id="combo-box-demo"
+      options={props.truckNames}
+      getOptionLabel={option => option}
+      filterSelectedOptions
+      style={{ width: 300 }}
+      onInputChange={handleInputChange}
+      renderInput={params => (
+        <TextField
+          {...params}
+          label="Combo box"
+          variant="outlined"
+          placeholder="Filter"
+        />
+      )}
     />
   );
 }
--- src/TimelineRenderer.tsx
+++ src/TimelineRenderer.tsx
@@ -1,11 +1,11 @@
 import { DataComponentInterface } from "./Types";
-import React, { useState, useEffect } from "react";
-import moment from "moment";
-import { Timeline, TimelineGroup, TimelineItem } from "react-calendar-timeline";
+import React, { useState, useEffect, ChangeEvent } from "react";
+import Timeline, { TimelineGroup, TimelineItem } from "react-calendar-timeline";
 // make sure you include the timeline stylesheet or the timeline will not be styled
 import "react-calendar-timeline/lib/Timeline.css";
 import DataConvertHelper from "./DataConvertHelper";
 import FilterBox from "./FilterBox";
+import moment from "moment";
 
 type TimelineRendererProps = {
   dataComponent: DataComponentInterface;
@@ -15,6 +15,7 @@ export default function TimelineRenderer(props: TimelineRendererProps) {
   const [groups, setGroups]: TimelineGroup[] = useState([]);
   const [items, setItems]: TimelineItem[] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [selectedGroups, updateSelectedGroups] = useState([]);
 
   useEffect(() => {
     props.dataComponent
@@ -34,15 +35,24 @@ export default function TimelineRenderer(props: TimelineRendererProps) {
       .catch(error => console.log(error));
   }, [props.dataComponent]);
 
+  const handleInputChange = (
+    event: ChangeEvent<{}>,
+    newInput: string[],
+    reason: "input" | "reset" | "clear"
+  ) => {
+    updateSelectedGroups(newInput);
+  };
+
   return isLoading ? (
     "Loading Data..."
   ) : (
     <div>
       <FilterBox
         truckNames={groups.map((group: TimelineGroup) => group.title)}
+        onInputChange={handleInputChange}
       />
       <Timeline
-        groups={groups}
+        groups={selectedGroups}
         items={items}
         defaultTimeStart={moment("2020.02.01 0:00:00")}
         defaultTimeEnd={moment("2020.02.03 0:00:00")}
